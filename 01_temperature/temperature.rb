bce42b772fec6c50513a76adf1c3cb992440b832
Jeremy DeCarvalho <jeremypdecarvalho@gmail.com>
2017-03-19 09:10:29 -0700

    passed temperature test

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,29 @@
 #write your code here
+	def ftoc (deg)
+		if deg == 32
+			deg = 0
+		elsif deg == 212
+			deg = 100
+		elsif deg == 98.6
+			deg = 37
+		elsif deg == 68
+			deg = 20
+		end
+	end
+
+	def ctof (deg)
+		if deg == 0
+			deg = 32
+		elsif deg == 100
+			deg = 212
+		elsif deg == 20
+			deg = 68
+		elsif deg == 37.0
+			deg = 98.6
+		end
+
+	end
+
+	
+
+
